1d700a9789967e29b389cfb4da7b5af1f50c5d3f
psopher <phsophe@gmail.com>
2018-01-18 19:48:03 +0100

    forms work

---
+++ scripts/slider.js
@@ -0,0 +1,371 @@
+/*Slider 1*/
+
+var slider_1 = document.getElementById("question-1-id");
+var output_1 = document.getElementById("question-1-value");
+output_1.innerHTML = slider_1.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_1.oninput = function() {
+    output_1.innerHTML = this.value;
+}
+
+/*Slider 2*/
+
+var slider_2 = document.getElementById("question-2-id");
+var output_2 = document.getElementById("question-2-value");
+output_2.innerHTML = slider_2.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_2.oninput = function() {
+    output_2.innerHTML = this.value;
+}
+
+/*Slider 3*/
+
+var slider_3 = document.getElementById("question-3-id");
+var output_3 = document.getElementById("question-3-value");
+output_3.innerHTML = slider_3.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_3.oninput = function() {
+    output_3.innerHTML = this.value;
+}
+
+/*Slider 4*/
+
+var slider_4 = document.getElementById("question-4-id");
+var output_4 = document.getElementById("question-4-value");
+output_4.innerHTML = slider_4.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_4.oninput = function() {
+    output_4.innerHTML = this.value;
+}
+
+/*Slider 5*/
+
+var slider_5 = document.getElementById("question-5-id");
+var output_5 = document.getElementById("question-5-value");
+output_5.innerHTML = slider_5.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_5.oninput = function() {
+    output_5.innerHTML = this.value;
+}
+
+/*Slider 6*/
+
+var slider_6 = document.getElementById("question-6-id");
+var output_6 = document.getElementById("question-6-value");
+output_6.innerHTML = slider_6.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_6.oninput = function() {
+    output_6.innerHTML = this.value;
+}
+
+/*Slider 7*/
+
+var slider_7 = document.getElementById("question-7-id");
+var output_7 = document.getElementById("question-7-value");
+output_7.innerHTML = slider_7.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_7.oninput = function() {
+    output_7.innerHTML = this.value;
+}
+
+/*Slider 8*/
+
+var slider_8 = document.getElementById("question-8-id");
+var output_8 = document.getElementById("question-8-value");
+output_8.innerHTML = slider_8.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_8.oninput = function() {
+    output_8.innerHTML = this.value;
+}
+
+/*Slider 9*/
+
+var slider_9 = document.getElementById("question-9-id");
+var output_9 = document.getElementById("question-9-value");
+output_9.innerHTML = slider_9.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_9.oninput = function() {
+    output_9.innerHTML = this.value;
+}
+
+/*Slider 10*/
+
+var slider_10 = document.getElementById("question-10-id");
+var output_10 = document.getElementById("question-10-value");
+output_10.innerHTML = slider_10.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_10.oninput = function() {
+    output_10.innerHTML = this.value;
+}
+
+/*Slider 11*/
+
+var slider_11 = document.getElementById("question-11-id");
+var output_11 = document.getElementById("question-11-value");
+output_11.innerHTML = slider_11.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_11.oninput = function() {
+    output_11.innerHTML = this.value;
+}
+
+/*Slider 12*/
+
+var slider_12 = document.getElementById("question-12-id");
+var output_12 = document.getElementById("question-12-value");
+output_12.innerHTML = slider_12.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_12.oninput = function() {
+    output_12.innerHTML = this.value;
+}
+
+/*Slider 13*/
+
+var slider_13 = document.getElementById("question-13-id");
+var output_13 = document.getElementById("question-13-value");
+output_13.innerHTML = slider_13.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_13.oninput = function() {
+    output_13.innerHTML = this.value;
+}
+
+/*Slider 14*/
+
+var slider_14 = document.getElementById("question-14-id");
+var output_14 = document.getElementById("question-14-value");
+output_14.innerHTML = slider_14.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_14.oninput = function() {
+    output_14.innerHTML = this.value;
+}
+
+/*Slider 15*/
+
+var slider_15 = document.getElementById("question-15-id");
+var output_15 = document.getElementById("question-15-value");
+output_15.innerHTML = slider_15.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_15.oninput = function() {
+    output_15.innerHTML = this.value;
+}
+
+/*Slider 16*/
+
+var slider_16 = document.getElementById("question-16-id");
+var output_16 = document.getElementById("question-16-value");
+output_16.innerHTML = slider_16.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_16.oninput = function() {
+    output_16.innerHTML = this.value;
+}
+
+/*Slider 17*/
+
+var slider_17 = document.getElementById("question-17-id");
+var output_17 = document.getElementById("question-17-value");
+output_17.innerHTML = slider_17.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_17.oninput = function() {
+    output_17.innerHTML = this.value;
+}
+
+/*Slider 18*/
+
+var slider_18 = document.getElementById("question-18-id");
+var output_18 = document.getElementById("question-18-value");
+output_18.innerHTML = slider_18.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_18.oninput = function() {
+    output_18.innerHTML = this.value;
+}
+
+/*Slider 19*/
+
+var slider_19 = document.getElementById("question-19-id");
+var output_19 = document.getElementById("question-19-value");
+output_19.innerHTML = slider_19.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_19.oninput = function() {
+    output_19.innerHTML = this.value;
+}
+
+/*Slider 20*/
+
+var slider_20 = document.getElementById("question-20-id");
+var output_20 = document.getElementById("question-20-value");
+output_20.innerHTML = slider_20.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_20.oninput = function() {
+    output_20.innerHTML = this.value;
+}
+
+/*Slider 21*/
+
+var slider_21 = document.getElementById("question-21-id");
+var output_21 = document.getElementById("question-21-value");
+output_21.innerHTML = slider_21.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_21.oninput = function() {
+    output_21.innerHTML = this.value;
+}
+
+/*Slider 22*/
+
+var slider_22 = document.getElementById("question-22-id");
+var output_22 = document.getElementById("question-22-value");
+output_22.innerHTML = slider_22.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_22.oninput = function() {
+    output_22.innerHTML = this.value;
+}
+
+/*Slider 23*/
+
+var slider_23 = document.getElementById("question-23-id");
+var output_23 = document.getElementById("question-23-value");
+output_23.innerHTML = slider_23.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_23.oninput = function() {
+    output_23.innerHTML = this.value;
+}
+
+/*Slider 24*/
+
+var slider_24 = document.getElementById("question-24-id");
+var output_24 = document.getElementById("question-24-value");
+output_24.innerHTML = slider_24.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_24.oninput = function() {
+    output_24.innerHTML = this.value;
+}
+
+/*Slider 25*/
+
+var slider_25 = document.getElementById("question-25-id");
+var output_25 = document.getElementById("question-25-value");
+output_25.innerHTML = slider_25.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_25.oninput = function() {
+    output_25.innerHTML = this.value;
+}
+
+/*Slider 26*/
+
+var slider_26 = document.getElementById("question-26-id");
+var output_26 = document.getElementById("question-26-value");
+output_26.innerHTML = slider_26.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_26.oninput = function() {
+    output_26.innerHTML = this.value;
+}
+
+/*Slider 27*/
+
+var slider_27 = document.getElementById("question-27-id");
+var output_27 = document.getElementById("question-27-value");
+output_27.innerHTML = slider_27.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_27.oninput = function() {
+    output_27.innerHTML = this.value;
+}
+
+/*Slider 28*/
+
+var slider_28 = document.getElementById("question-28-id");
+var output_28 = document.getElementById("question-28-value");
+output_28.innerHTML = slider_28.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_28.oninput = function() {
+    output_28.innerHTML = this.value;
+}
+
+/*Slider 29*/
+
+var slider_29 = document.getElementById("question-29-id");
+var output_29 = document.getElementById("question-29-value");
+output_29.innerHTML = slider_29.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_29.oninput = function() {
+    output_29.innerHTML = this.value;
+}
+
+/*Slider 30*/
+
+var slider_30 = document.getElementById("question-30-id");
+var output_30 = document.getElementById("question-30-value");
+output_30.innerHTML = slider_30.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_30.oninput = function() {
+    output_30.innerHTML = this.value;
+}
+
+/*Slider 31*/
+
+var slider_31 = document.getElementById("question-31-id");
+var output_31 = document.getElementById("question-31-value");
+output_31.innerHTML = slider_31.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider_31.oninput = function() {
+    output_31.innerHTML = this.value;
+}
+
+/*
+var sliders = [];
+var outputs = [];
+for (i = 0; i < 31; i++) {
+
+    sliders[i] = document.getElementById("question-" + (i+1) + "-id");
+    outputs[i] = document.getElementById("question-" + (i+1) + "-value");
+    outputs[i].innerHTML = sliders[i].value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    sliders[i].oninput = function() {
+        console.log(i);
+        console.log(variableName(sliders[i]));
+        console.log(outputs[i]);
+        console.log(this.value);
+        outputs[i].innerHTML = this.value;
+    }
+}
+
+for (i = 1; i < 32; i++) {
+    this["slider"+i] = document.getElementById("question-" + i + "-id");
+    this["output"+i] = document.getElementById("question-" + i + "-value");
+    outputs[i].innerHTML = sliders[i].value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    sliders[i].oninput = function() {
+        outputs[i].innerHTML = this.value;
+    }
+}
+*/
